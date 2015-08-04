d06300f32a7f55ad875e4d23639cf70ac155d2d8
krisdb <kris@qualcode.net>
2015-08-04 16:19:07 -0400

    Initial commit

---
+++ app/src/main/java/com/krisdb/quickmarks/BookmarkItem.java
@@ -0,0 +1,39 @@
+package com.krisdb.quickmarks;
+
+public class BookmarkItem implements Comparable<BookmarkItem> {
+
+    private String title, url;
+    private byte[] favicon;
+
+    public String getTitle() {
+        return title;
+    }
+    public byte[] getFavicon() {
+        return favicon;
+    }
+    public String getUrl() { return url; }
+
+    public void setTitle(final String title) {
+        this.title = title.trim();
+    }
+    public void setFavicon(final byte[] favicon) {
+        this.favicon = favicon;
+    }
+    public void setUrl(final String url)  {
+        this.url = url.trim();
+    }
+
+    public BookmarkItem copy(){
+        final BookmarkItem copy = new BookmarkItem();
+        copy.title = title;
+        copy.url = url;
+        copy.favicon = favicon;
+        return copy;
+    }
+
+    public int compareTo(final BookmarkItem another) {
+        if (another == null) return 1;
+        // sort descending, most recent first
+        return another.title.compareTo(title);
+    }
+}
---
+++ app/src/main/java/com/krisdb/quickmarks/BookmarkList.java
@@ -0,0 +1,161 @@
+package com.krisdb.quickmarks;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.Browser;
+import android.support.v4.content.ContextCompat;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BookmarkList extends ListActivity {
+    protected CustomAdapter adapter;
+    private List<BookmarkItem> bookmarks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.bookmark_list);
+
+        adapter = new CustomAdapter();
+        bookmarks = new ArrayList<>();
+
+        final PackageManager pm = getPackageManager();
+
+        final ResolveInfo mInfo = pm.resolveActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com")), 0);
+
+        String browserURi;
+
+        final String packageName = String.valueOf(pm.getApplicationLabel(mInfo.activityInfo.applicationInfo)).toLowerCase();
+
+        if (packageName.contains("firefox"))
+            browserURi = getResources().getString(R.string.content_uri_firefox);
+        else if(packageName.contains("chrome"))
+            browserURi = getResources().getString(R.string.content_uri_chrome);
+        else
+            browserURi =  getResources().getString(R.string.content_uri_default);
+
+        String[] projection = new String[] {Browser.BookmarkColumns.FAVICON, Browser.BookmarkColumns.TITLE, Browser.BookmarkColumns.URL};
+        Uri uri = Uri.parse(browserURi);
+        final  Cursor cursor = getContentResolver().query(uri, projection, Browser.BookmarkColumns.BOOKMARK + " = 1", null, null);
+
+        if (cursor == null) return;
+
+        if (cursor.moveToFirst())
+        {
+            while (!cursor.isAfterLast()) {
+
+                BookmarkItem bm = new BookmarkItem();
+                bm.setTitle(cursor.getString(cursor.getColumnIndex(Browser.BookmarkColumns.TITLE)));
+                bm.setUrl(cursor.getString(cursor.getColumnIndex(Browser.BookmarkColumns.URL)));
+                bm.setFavicon(cursor.getBlob(cursor.getColumnIndex(Browser.BookmarkColumns.FAVICON)));
+
+                adapter.addItem(bm);
+                bookmarks.add(bm.copy());
+
+                cursor.moveToNext();
+            }
+        }
+
+        if (cursor.isClosed() == false)
+            cursor.close();
+
+        setListAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id)
+    {
+        super.onListItemClick(l, v, position, id);
+
+        final BookmarkItem bookmark = bookmarks.get(position);
+
+        startActivityForResult(new Intent(Intent.ACTION_VIEW, Uri.parse(bookmark.getUrl())), 1);
+    }
+
+    /* ADAPTER */
+    private class CustomAdapter extends BaseAdapter
+    {
+        private final List<BookmarkItem> bookmarks = new ArrayList<>();
+        private final LayoutInflater mInflater;
+
+        public CustomAdapter() {
+            mInflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+        }
+
+        public void addItem(BookmarkItem bookmark) {
+            bookmarks.add(bookmark);
+        }
+
+        @Override
+        public int getCount() {
+            return bookmarks.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return bookmarks.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, final ViewGroup parent)
+        {
+            final ViewHolder holder;
+            final BookmarkItem bookmark = (BookmarkItem)getItem(position);
+
+            if (convertView == null)
+            {
+                holder = new ViewHolder();
+
+                convertView = mInflater.inflate(R.layout.bookmark_list_item, null);
+                holder.bookmark_item_title = (TextView)convertView.findViewById(R.id.bookmark_item_title);
+                holder.bookmark_item_favicon = (ImageView)convertView.findViewById(R.id.bookmark_item_favicon);
+
+                convertView.setTag(holder);
+            }
+            else
+                holder = (ViewHolder)convertView.getTag();
+
+            holder.bookmark_item_title.setText(bookmark.getTitle());
+
+            if (bookmark.getFavicon() != null)
+                holder.bookmark_item_favicon.setImageBitmap(getImage(bookmark.getFavicon()));
+            else
+                holder.bookmark_item_favicon.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.icon_bookmark));
+
+            return convertView;
+        }
+    }
+
+    static class ViewHolder {
+        TextView bookmark_item_title;
+        ImageView bookmark_item_favicon;
+    }
+   /* ADAPTER */
+
+    public static Bitmap getImage(byte[] image) {
+        return BitmapFactory.decodeByteArray(image, 0, image.length);
+    }
+}
